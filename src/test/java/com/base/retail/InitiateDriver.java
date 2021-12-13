cb94699ffb32e3f79a17b46ec2c12f4d32b2022a
kishorere <chselenium4web@gmail.com>
2021-12-13 21:15:53 +0530

    Commit Second assignment
    Commit Second assignment

---
+++ src/test/java/com/base/retail/InitiateDriver.java
@@ -0,0 +1,28 @@
+package com.base.retail;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import com.mkmytrip.common.ReadConfig;
+
+public class InitiateDriver {
+	public static WebDriver driver;
+	
+	public static void startDriverInstance() throws Exception {
+		String browser = ReadConfig.readProjectConfig("BrowserName");
+		if(browser.equalsIgnoreCase("Firefox")) {
+			System.setProperty(ReadConfig.readProjectConfig("FF_Key"), ReadConfig.readProjectConfig("FF_Path"));
+			driver = new FirefoxDriver();
+		}
+		driver.manage().window().maximize();
+		driver.get(ReadConfig.readProjectConfig("ApplicationURL"));
+		Thread.sleep(3000);
+		//driver.findElement(By.xpath("//div[@class='loginModal displayBlock modalLogin dynHeight personal ']")).click();
+	}
+	
+	public WebDriver getDriverInstance() throws Exception {
+		InitiateDriver.startDriverInstance();
+		return driver;
+	}
+
+}
